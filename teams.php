3016c7e87246d6f90253464b2730670e10ee560a
giovanniadinolfi <g.adinolfi39@studenti.unisa.it>
2025-02-11 17:39:07 +0100

    teams completed

--- teams.php
+++ teams.php
@@ -8,11 +8,11 @@
     <body>
         <?php include 'header.html'; ?>
         <nav style="text-align: center; background-color: #b0c4de; height: 50px; padding-top: 15px;">
-            <a href="" style="color: inherit; text-decoration: none;">PRIMA SQUADRA MASCHILE</a> | 
-            <a href="" style="color: inherit; text-decoration: none;"> U19 SILVER</a> | 
-            <a href="" style="color: inherit; text-decoration: none;"> U17 LIBERTAS</a> |
-            <a href="" style="color: inherit; text-decoration: none;"> U13 LIBERTAS</a> |
-            <a href="" style="color: inherit; text-decoration: none;"> MINIBASKET</a>
+            <a href="teams.php" style="color: inherit; text-decoration: none;">PRIMA SQUADRA MASCHILE</a> | 
+            <a href="u19silver.php" style="color: inherit; text-decoration: none;"> U19 SILVER</a> | 
+            <a href="u17libertas.php" style="color: inherit; text-decoration: none;"> U17 LIBERTAS</a> |
+            <a href="u13libertas.php" style="color: inherit; text-decoration: none;"> U13 LIBERTAS</a> |
+            <a href="minibasket.php" style="color: inherit; text-decoration: none;"> MINIBASKET</a>
         </nav>
         <div style="padding-top: 15px"> <Strong>PRIMA SQUADRA MASCHILE</Strong></div> <br>
         <nav>
---
+++ u17libertas.php
@@ -0,0 +1,90 @@
+<html>
+    <head>
+        <title>U17 Libertas</title>
+        <link rel="stylesheet" href="css_teams.css" type="text/css"/>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+            text-align: center;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+        }
+        th {
+            background-color: #4CAF50; /* Colore diverso per la prima riga */
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2; /* Alternanza di colori per le righe */
+        }
+        </style>
+    </head>
+    <body>
+        <?php include 'header.html'; ?>
+        <nav style="text-align: center; background-color: #b0c4de; height: 50px; padding-top: 15px;">
+            <a href="teams.php" style="color: inherit; text-decoration: none;">PRIMA SQUADRA MASCHILE</a> | 
+            <a href="u19silver.php" style="color: inherit; text-decoration: none;"> U19 SILVER</a> | 
+            <a href="U17libertas.php" style="color: inherit; text-decoration: none;"> U17 LIBERTAS</a> |
+            <a href="u13libertas.php" style="color: inherit; text-decoration: none;"> U13 LIBERTAS</a> |
+            <a href="minibasket.php" style="color: inherit; text-decoration: none;"> MINIBASKET</a>
+        </nav>
+        <div style="padding-top: 15px"> <Strong>UNDER 17 LIBERTAS</Strong></div> <br>
+        <br>
+        <div style="align: center"> <img src="logo.png" alt="Foto Squadra"></div>
+        <br>
+        <div style="text-align: center"> <strong>U17 LIBERTAS</strong><br>Ecco la rosa ufficiale della stagione 2024/25</div>
+        <table>
+        <tr>
+            <th>Colonna 1</th>
+            <th>Colonna 2</th>
+            <th>Colonna 3</th>
+            <th>Colonna 4</th>
+        </tr>
+        <tr>
+            <td>Dato 1</td>
+            <td>Dato 2</td>
+            <td>Dato 3</td>
+            <td>Dato 4</td>
+        </tr>
+        <tr>
+            <td>Dato 5</td>
+            <td>Dato 6</td>
+            <td>Dato 7</td>
+            <td>Dato 8</td>
+        </tr>
+        <tr>
+            <td>Dato 9</td>
+            <td>Dato 10</td>
+            <td>Dato 11</td>
+            <td>Dato 12</td>
+        </tr>
+    </table>
+        ALLENATORE
+        <div style="padding-top: 15px" class="geppetto">
+                <div class="image-container">
+                    <img src="mino.png" alt="immagine" class="image">
+                </div>
+            <div class="text-container">
+            <div class="text-block">
+                <p><strong>JACOPO PORPORA</strong></p>
+                <p><strong>DATA DI NASCITA:</strong> NON LO SO</p>
+                <p><strong>LUOGO DI NASCITA:</strong> PENSO MINORI</p>
+                <p><strong>NAZIONALITÀ:</strong> MINORESE</p>
+                <p><strong>ALLENATORE DAL:</strong> 1945</p>
+                <p><strong>AL GSMINORI DAL:</strong> 2003</p>
+            </div>
+            <div class="text-block">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima sequi cupiditate repudiandae, 
+                    vero tenetur dolorum architecto provident deserunt quos dolore maiores illum a dolorem impedit 
+                    temporibus sit dolor consectetur distinctio?</p>
+            </div>
+        </div>
+        </div>
+           <?php include 'footer.html'; ?>
+    </body>
+</html>
