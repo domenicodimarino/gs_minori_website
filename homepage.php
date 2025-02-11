9f83a586db914e4893f92ac139946e126e311bc1
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2025-02-11 12:45:37 +0100

    footer

--- homepage.php
+++ homepage.php
@@ -2,8 +2,10 @@
     <head>
         <title>GS Minori - Sito ufficiale</title>
         <link rel="stylesheet" href="style.css" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
     <body>
         <?php include 'header.html'?>
+        <?php include 'footer.html'?>
     </body>
 </html>
