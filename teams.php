a170d8f1d25040966cabe30c995998ef1aee3d4a
giovanniadinolfi <g.adinolfi39@studenti.unisa.it>
2025-02-11 15:59:23 +0100

    squadra principale

--- teams.php
+++ teams.php
@@ -1,10 +1,71 @@
 <html>
     <head>
         <title>Teams</title>
+        <link rel="stylesheet" href="css_teams.css" type="text/css"/>
         <link rel="stylesheet" href="style.css" type="text/css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
     <body>
         <?php include 'header.html'; ?>
+        <nav style="text-align: center; background-color: #b0c4de; height: 50px; padding-top: 15px;">
+            <a href="" style="color: inherit; text-decoration: none;">PRIMA SQUADRA MASCHILE</a> | 
+            <a href="" style="color: inherit; text-decoration: none;"> U19 SILVER</a> | 
+            <a href="" style="color: inherit; text-decoration: none;"> U17 LIBERTAS</a> |
+            <a href="" style="color: inherit; text-decoration: none;"> U13 LIBERTAS</a> |
+            <a href="" style="color: inherit; text-decoration: none;"> MINIBASKET</a>
+        </nav>
+        <div style="padding-top: 15px"> <Strong>PRIMA SQUADRA MASCHILE</Strong></div> <br>
+        <nav>
+            <a href="#playmaker" style="color: inherit; text-decoration: none;"> PLAYMAKER</a> | 
+            <a href="#guardia" style="color: inherit; text-decoration: none;"> GUARDIA</a> | 
+            <a href="#alapiccola" style="color: inherit; text-decoration: none;"> ALA PICCOLA</a> |
+            <a href="#alagrande" style="color: inherit; text-decoration: none;"> ALA GRANDE</a> |
+            <a href="#centro" style="color: inherit; text-decoration: none;"> CENTRO</a> |
+            <a href="#allenatore" style="color: inherit; text-decoration: none;"> ALLENATORE</a>
+        </nav>
+        PLAYMAKER
+            <div id="playmaker"; style="padding-top: 15px">
+                    <div class="image-container">
+                    <img style="padding-top: 15px" src="mino.png" alt="immagine" class="image">
+                    <div class="text"> hola </div>
+                    </div>
+            </div>
+        GUARDIA
+            <div id="guardia" style="padding-top: 15px" class="geppetto">
+            
+                <div class="image-container">
+                    <img src="mino.png" alt="immagine" class="image">
+                    <div class="text"> hola</div>
+                </div>
+                <div class="image-container">
+                    <img style="padding-top: 15px" src="logo.png" alt="immagine" class="image">
+                    <div class="text"> hola1 <br>
+                        dom <br>
+                        dom
+                    </div>
+                </div>
+            </div>
+        ALA PICCOLA
+            <div id="alapiccola" style="padding-top: 15px">
+                <div class="image-container">
+                    <img src="mino.png" alt="immagine" class="image">
+                    <div class="text"> hola</div>
+                </div>
+            </div>
+        ALA GRANDE
+            <div id="alagrande"; style="padding-top: 15px">
+                <div class="image-container">
+                    <img src="mino.png" alt="immagine" class="image">
+                    <div class="text"> hola</div>
+                </div>
+            </div>
+        CENTRO
+            <div id="centro"; style="padding-top: 15px">
+                <div class="image-container">
+                    <img src="mino.png" alt="immagine" class="image">
+                    <div class="text"> hola</div>
+                </div>
+            </div>
+        <?php include 'footer.html'; ?>
     </body>
 </html>
