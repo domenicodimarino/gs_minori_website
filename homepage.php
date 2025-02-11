dfe923ce2e0b1fef3dd122de47fc847c6daab0a8
Di Marino Domenico <d.dimarino8@studenti.unisa.it>
2025-02-11 12:08:44 +0100

    header

---
+++ homepage.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>GS Minori - Sito ufficiale</title>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+    </head>
+    <body>
+        <?php include 'header.html'?>
+    </body>
+</html>
