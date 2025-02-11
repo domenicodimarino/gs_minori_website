49fdeddcd977f961f36cf7c85e692c09e7a3c1ee
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2025-02-11 15:51:53 +0100

    Club

--- club.php
+++ club.php
@@ -5,8 +5,17 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
     <body>
+        <link rel="stylesheet" href="club.css" type="text/css"/>
         <?php include 'header.html'?>
+        <body>
+            <h1>Chi siamo?</h1>
+
+            <h1>Staff</h1>
+
+            <h1>La Storia</h1>
+           
+        </body>
         <?php include 'footer.html'?>
     </body>
-    
+
 </html>
