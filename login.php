8f5f4bc80c134b00b1db9aabd502eb7eb99a6468
Di Marino Domenico <d.dimarino8@studenti.unisa.it>
2025-02-11 12:48:35 +0100

    login non completo

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>GS Minori - Login</title>
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+    </head>
+    <body style="align-items: center; display: flex; flex-direction: column;">
+        <main>
+            <img src="logo.png" alt="Logo GS Minori" style="width: 40vw;" height="auto">
+            <form method="post" action="login-manager.php" style="align-items: center; display: flex; flex-direction: column;">
+                <p>
+                <label for="email">Email</p><p>
+                    <input type="text" name="email" id="email"/>
+                </label>
+                </p>
+                <p>
+                <label for="password">Password</p><p>
+                    <input type="password" name="password" id="password"/>
+                </label>
+                </p>
+                <p>
+                <input type="submit" name="invia" value="Login"/>
+                </p>
+                <p> Nuovo utente? <a href="registrati.php">Registrati!</a></p>
+            </form>
+
+            
+    </main>
+    </body>
+</html>
