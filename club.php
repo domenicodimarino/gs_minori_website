c43dee98e44a5e6b956f3cabb81307fc7c3248b2
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2025-02-12 12:08:05 +0100

    club, in attesa delle info

--- club.php
+++ club.php
@@ -3,19 +3,33 @@
         <title>GS Minori - Sito ufficiale</title>
         <link rel="stylesheet" href="style.css" type="text/css"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="club.css" type="text/css"/>
     </head>
     <body>
-        <link rel="stylesheet" href="club.css" type="text/css"/>
         <?php include 'header.html'?>
-        <body>
-            <h1>Chi siamo?</h1>
+            <h1 id="Chi siamo">Chi siamo?</h1>
+            <p>Il Gruppo Sportivo Minori Pallacanestro (GS Minori) è una storica società sportiva della Costiera Amalfitana, attiva da oltre quarant'anni nel panorama cestistico campano. Fondata con l'obiettivo di promuovere la pallacanestro e i valori sportivi nella comunità locale, la società ha sempre posto l'accento sulla formazione dei giovani e sull'inclusione sociale.</p>
+            attiva da oltre quarant'anni nel panorama cestistico campano. Fondata con l'obiettivo di promuovere la pallacanestro 
+            e i valori sportivi nella comunità locale, la società ha sempre posto l'accento sulla formazione dei giovani e 
+            sull'inclusione sociale. </p>
+            <img src="photo_club/chi_siamo.jpg" alt="Chi" class="chi">
+            <h2>Mino</h2>
+            <p> Oltre all'attività agonistica, il GS Minori ha curato l'aspetto identitario e culturale del club. Nel 2019, è stata introdotta la mascotte ufficiale, "Mino", un toro marino ispirato a un mosaico della Villa Romana Marittima di Minori, simbolo del legame tra la società e il patrimonio storico locale.<img src="photo_club/mino_copy.png" alt="Mino" class="mino"></p>
+            
+            <h1 id="Staff">Staff</h1>
+            <p>Il GS Minori può contare su uno <strong>staff dirigenziale e tecnico di grande competenza e passione</strong>, impegnato nella crescita del movimento cestistico locale.<br>
+                Alla guida della società c'è il <strong>Presidente Vito D'Alfonso</strong>, affiancato dal <strong>Vicepresidente Ivo Di Lieto</strong> e dalla <strong>Dirigente Mariangela Di Lieto</strong>, figure chiave nella gestione organizzativa e nello sviluppo delle attività sportive.<br>
+                Il settore tecnico vanta un team di <strong>allenatori qualificati</strong>, tra cui <strong>Mimmo Apicella, Andrea Dipino, Federico Apicella, Marianna D'Amato, Jacopo Porpora, Giancarlo Di Bianco e Gabriele Di Lieto</strong>, professionisti dediti alla formazione dei giovani atleti e alla guida delle squadre nelle competizioni regionali.<br>
+                Grazie al loro impegno, il GS Minori continua a crescere, puntando su valori come dedizione, spirito di squadra e sviluppo del talento.</p>          
+            <h1 id="Storia">Storia</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis corporis, pariatur iste eos enim laborum impedit ea. At, tempore tenetur? Provident ad officia eligendi libero accusantium autem vero doloribus quis.
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae natus accusamus eos cupiditate ipsam beatae ipsum odio corrupti animi officia tempora eligendi eius deserunt temporibus doloremque quis, amet in voluptas?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae iure sunt porro deserunt voluptatem architecto ipsum. Deserunt nobis reiciendis aliquid perferendis perspiciatis repudiandae eligendi fuga error. Quia, cupiditate quos?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium nesciunt quaerat quisquam eligendi eveniet debitis saepe consequuntur impedit blanditiis ipsum reiciendis reprehenderit quidem ab, rem cumque amet! Voluptate, error nobis.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, dicta ab earum ea sapiente harum ut quod ex accusamus ullam obcaecati quasi quia dolorum iure adipisci vitae est cum rerum?
+            </p>
 
-            <h1>Staff</h1>
-
-            <h1>La Storia</h1>
-           
         </body>
         <?php include 'footer.html'?>
-    </body>
 
 </html>
