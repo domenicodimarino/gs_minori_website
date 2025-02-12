867224e31372b907848952f524e875d3a095f220
Di Marino Domenico <d.dimarino8@studenti.unisa.it>
2025-02-12 14:21:19 +0100

    mostra password e video sfondo

--- login.php
+++ login.php
@@ -6,11 +6,31 @@
 
             function registrazione(){
                 document.getElementById("login_form").style.display = "none";
-                document.getElementById("registration_form").style.display = "block";
+                document.getElementById("registration_form").style.display = "flex";
             }
             function login(){
-                document.getElementById("login_form").style.display = "block";
+                document.getElementById("login_form").style.display = "flex";
                 document.getElementById("registration_form").style.display = "none";
+            }
+            function mod_password(){
+                $type = document.getElementById('password').type;
+                if($type == "password"){
+                    document.getElementById('password').type = "text";
+                    document.getElementById('pwd1').type = "text";
+                    document.getElementById('confirm_password').type = "text";
+                    document.getElementById('pass_photo').src = "eyeopen.png";
+                    document.getElementById('pass_photo2').src = "eyeopen.png";
+                    document.getElementById('pass_photo3').src = "eyeopen.png";
+                }    
+                else{
+                    document.getElementById('password').type = "password";
+                    document.getElementById('pwd1').type = "password";
+                    document.getElementById('confirm_password').type = "password";
+                    document.getElementById('pass_photo').src = "eyeclosed.png";
+                    document.getElementById('pass_photo2').src = "eyeclosed.png";
+                    document.getElementById('pass_photo3').src = "eyeclosed.png";
+                }
+                    
             }
             function check_signup_data(elementoModulo) {
                 if (elementoModulo.name.value == "") {
@@ -65,68 +85,76 @@
 		$repassword = "";
 
 ?>
-        <main style="align-items: center; display: flex; flex-direction: column;">
-            <a href="homepage.php"><img src="logo.png" alt="Logo GS Minori" style="width: 40vw;" height="auto"></a>
-            <div id="login_form" style="display:block;">
-            <form method="post" action=<?php echo $_SERVER['PHP_SELF']?> style="align-items: center; display: flex; flex-direction: column;">
-                <p>
-                <label for="email">
-                    <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $email?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="password">
-                    <input type="password" name="password" id="password" placeholder="Password" value="<?php echo $pass?>"/>
-                </label>
-                </p>
-                <p>
-                <input type="submit" name="login" class="submit_button" value="Login"/>
-                </p>
-                <p> Nuovo utente?</p>
-                <p><input type="button" name="go_to_sign_up" class="submit_button" value="Registrati!" onclick="registrazione()"/></p>
-            </form>
-            </div>
-            
-            <div id="registration_form" style="display:none;">
-            <form method="post" action=<?php echo $_SERVER['PHP_SELF'] ?> style="align-items: center; display: flex; flex-direction: column;" onsubmit="return check_signup_data(this)">
-                <p>
-                <label for="name">
-                    <input type="text" name="name" id="name" placeholder="Nome" value="<?php echo $name?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="surname">
-                    <input type="text" name="surname" id="surname" placeholder="Cognome" value="<?php echo $surname?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="username">
-                    <input type="text" name="username" id="username" placeholder="Username" value="<?php echo $username?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="email">
-                    <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $email?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="password">
-                    <input type="password" name="password" id="password" placeholder="Password" value="<?php echo $pass?>"/>
-                </label>
-                </p>
-                <p>
-                <label for="confirm_password">
-                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Conferma password" value="<?php echo $repassword?>"/>
-                </label>
-                </p>
-                <p>
-                <input type="submit" name="sign_in" class="submit_button" value="Effettua la registrazione"/>
-                </p>
-                <p> Sei già registrato?</p>
-                <p><input type="button" name="go_to_login" class="submit_button" value="Effettua il login!" onclick="login()"/></p>
-            </form>
-            </div>
-            <a href="homepage.php"><button>Torna alla homepage</button></a>
+<video src="spalti.mp4" autoplay loop muted style="position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%; opacity: 0.5; z-index: -1;">
+    Video cannot be displayed.
+    </video>
+    <main style="align-items: center; display: flex; flex-direction: column; ">
+        <a href="homepage.php"><img src="logo.png" alt="Logo GS Minori" style="width: 40vw;" height="auto"></a>
+        <!-- Form di login MODIFICARE ACTION -->
+        <div id="login_form" style="display:flex;">
+        <form method="post" action=<?php echo $_SERVER['PHP_SELF']?>>
+        <p>
+        <label for="email">
+            <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $email?>"/>
+        </label>
+        </p>
+        <p>
+        <label for="password" style="display: flex; align-items: center;">
+            <input type="password" name="password" id="password" placeholder="Password" value="<?php echo $pass?>" style="margin-right: 5px";/>
+            <img id="pass_photo" src="eyeclosed.png" alt="Mostra password" style="width: 20px;" onclick="mod_password()"/>
+        </label>
+        </p>
+        <p>
+        <input type="submit" name="login" class="submit_button" value="Login"/>
+        </p>
+        <p> Nuovo utente?</p>
+        <p><input type="button" name="go_to_sign_up" class="submit_button" value="Registrati!" onclick="registrazione()"/></p>
+        </form>
+        </div>
+        
+        <div id="registration_form" style="display:none;">
+        <!-- Form di registrazione DA MODIFICARE ACTION -->
+        <form method="post" action=<?php echo $_SERVER['PHP_SELF'] ?> onsubmit="return check_signup_data(this)">
+        <p>
+        <label for="name">
+            <input type="text" name="name" id="name" placeholder="Nome" value="<?php echo $name?>"/>
+        </label>
+        </p>
+        <p>
+        <label for="surname">
+            <input type="text" name="surname" id="surname" placeholder="Cognome" value="<?php echo $surname?>"/>
+        </label>
+        </p>
+        <p>
+        <label for="username">
+            <input type="text" name="username" id="username" placeholder="Username" value="<?php echo $username?>"/>
+        </label>
+        </p>
+        <p>
+        <label for="email">
+            <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $email?>"/>
+        </label>
+        </p>
+        <p>
+        <label for="password" style="display: flex; align-items: center;">
+            <input type="password" name="pwd1" id="pwd1" placeholder="Password" value="<?php echo $pass?>"/>
+            <img id="pass_photo2" src="eyeclosed.png" alt="Mostra password" style="width: 20px;" onclick="mod_password()"/>
+        </label>
+        </p>
+        <p>
+        <label for="confirm_password" style="display: flex; align-items: center;">
+            <input type="password" name="confirm_password" id="confirm_password" placeholder="Conferma password" value="<?php echo $repassword?>"/>
+            <img id="pass_photo3" src="eyeclosed.png" alt="Mostra password" style="width: 20px;" onclick="mod_password()"/>
+        </label>
+        </p>
+        <p>
+        <input type="submit" name="sign_in" class="submit_button" value="Effettua la registrazione"/>
+        </p>
+        <p> Sei già registrato?</p>
+        <p><input type="button" name="go_to_login" class="submit_button" value="Effettua il login!" onclick="login()"/></p>
+        </form>
+        </div>
+        <a href="homepage.php"><button>Torna alla homepage</button></a>
     </main>
     </body>
 </html>
