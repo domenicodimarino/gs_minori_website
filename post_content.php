3f2d5a6c03491b9757297ec1818c57963ba3e8a6
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2025-02-15 17:36:59 +0100

    FAN: pubblicazione contenuti corretta

--- post_content.php
+++ post_content.php
@@ -22,8 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['content'], $_POST['nom
 
     // Gestione dell'upload dell'immagine
     if (isset($_FILES['image']) && $_FILES['image']['error'] == UPLOAD_ERR_OK) {
-      
-        $uploadDir = '/uploads/';
+        $uploadDir = 'uploads/';
         $imagePath = $uploadDir . basename($_FILES['image']['name']);
         if (!move_uploaded_file($_FILES['image']['tmp_name'], $imagePath)) {
             die("Error uploading the image.");
