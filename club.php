856e1bacc86e2700517dd71fce7c72082a0090aa
gabrieledilieto1 <g.dilieto5@studenti.unisa.it>
2025-02-18 13:03:03 +0100

    CLUB: definitiva

--- club.php
+++ club.php
@@ -1,6 +1,4 @@
-<?php
-  // Puoi eventualmente aggiungere qui eventuali variabili PHP se necessarie
-?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -9,6 +7,8 @@
   <link rel="stylesheet" href="style.css" type="text/css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link rel="stylesheet" href="club.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Great+Vibes&family=Sacramento&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <!-- Header -->
@@ -46,18 +46,20 @@
     
     Sul fronte della formazione giovanile, il <strong>Responsabile del Settore Giovanile, Apicella Mimmo</strong> è un punto di riferimento per la crescita delle nuove leve, seguendole con attenzione e dedizione.<br><br>
     
-    In ambito tecnico, la guida della <strong>Prima Squadra</strong> e delle categorie <strong>U19 e U17</strong> è affidata a <strong>Porpora Jacopo</strong>, un allenatore che si distingue per la capacità di trasmettere passione e disciplina, fondamentale per preparare gli atleti alle sfide competitive.<br><br>
+    In ambito tecnico, la guida della <strong>Prima Squadra</strong> e delle categorie <strong>U19 e U17</strong> è affidata a <strong>Porpora Jacopo</strong>, un allenatore che si distingue per la capacità di trasmettere passione, fondamentale per preparare gli atleti alle sfide competitive.<br><br>
     
     Per gli <strong>Under 13</strong>, il ruolo di allenatore spetta a <strong>Mimmo Apicella</strong>, che, grazie alla sua esperienza, ha saputo creare un ambiente formativo positivo e stimolante, affiancato dall'assistenza di <strong>Di Lieto Gabriele</strong>, il cui contributo è essenziale per affinare le competenze tecniche e tattiche dei giovani.<br><br>
     
     A completare il quadro, la <strong>Preparatrice Atletica D'Amato Marianna</strong> lavora instancabilmente per garantire che ogni atleta mantenga livelli ottimali di condizione fisica, elementi imprescindibili per affrontare le sfide in campo.<br><br>
     
-    Infine, il settore <strong>MiniBasket</strong>, fondamentale per avvicinare i più piccoli al mondo della pallacanestro, è coordinato da <strong>Dipino Andrea</strong> e <strong>Apicella Federico</strong>, supportati dall'<strong>Assistente MiniBasket Di Bianco Giancarlo</strong>, che insieme instaurano un clima di entusiasmo e di apprendimento, preparando le future generazioni con passione e professionalità.<br><br>
+    Infine, il settore <strong>MiniBasket</strong>, <img src="photo_club/andrea&Federico.jpeg" alt="" class="mini">fondamentale per avvicinare i più piccoli al mondo della pallacanestro, è coordinato da <strong>Dipino Andrea</strong> e <strong>Apicella Federico</strong>, supportati dall'<strong>Assistente MiniBasket Di Bianco Giancarlo</strong>, che insieme instaurano un clima di entusiasmo e di apprendimento, preparando le future generazioni con passione e professionalità.<br><br>
     
     In sintesi, lo staff del GS Minori è una squadra coesa e versatile, capace di coniugare <strong>esperienza, innovazione e amore per il gioco</strong>, elementi che rendono il club un punto di riferimento nella pallacanestro della Costiera Amalfitana.
   </p>
 </section>
+</div>
 
+<div class="container">
 
    <!-- Sezione "Storia"  -->
 <section id="storia" class="section-storia">
@@ -103,20 +105,46 @@
         "content" => "Nel frattempo cambiarono anche i presidenti. Alfonso Pepe, dopo aver raccolto grandissime soddisfazioni anche con il nostro gruppo 1963-1964 (rimasto imbattuto nel campionato ragazzi, fino ad arrivare alla fase precedente alle finali nazionali, perdendo solo in finale a Monopoli), lasciò la carica di presidente a Giovanni Ruocco, che fu poi fondamentale per la realizzazione della copertura del campo."
       ),
       array(
-        "title" => "La squadra femminile in Serie C e la Promozione maschile",
+        "date" => "1979",
+        "title" => "La squadra femminile e <br> Il sogno della finale con Italsider Napol",
         "content" => "La squadra femminile militava stabilmente in Serie C, mentre svolgevamo il campionato di Promozione maschile con buoni risultati. L’anno dopo, il nostro gruppo, formato da Enzo Di Palma (forse il talento più grande che abbiamo avuto nel settore maschile), Roberto Cafasso, Mimmo Tozzi e tanti altri ragazzi (tutti importantissimi, ma non citati qui per motivi di spazio), arrivammo alla finale contro Italsider Napoli, società che militava in B (paragonabile all’attuale A2 di oggi), per l’accesso alle finali nazionali di Montecatini. Se avessimo vinto, avremmo giocato con Bologna, Milano: un sogno che però si infranse con una sconfitta di sole cinque punti di scarto, con un arbitraggio scandaloso.",
         "image" => "photo_club/storia2.jpg"
       ),
       array(
         "date" => "Anni '80",
-        "title" => "Evoluzione e Boom",
-        "content" => "Avviandoci verso gli anni 1980, anni di boom per la pallacanestro, ricordo che noi svolgevamo tutti i campionati maschili e femminili con ottimi risultati. Pio Zizzi era il vero motore dell’associazione, attorno a lui iniziavano a crescere nuovi coach e istruttori. Rosa Ruocco fu ceduta a Gragnano nella A1 femminile e, in seguito, continuando l’ottima tradizione femminile, anche Liliana Miccio ha costruito un’ottima carriera in Serie A ed è attualmente top player di Firenze in A2."
+        "title" => "Boom della pallacanestro e l’accordo con <br> il C.S. Amalfitano",
+        "content" => "Avviandoci verso gli anni 1980, anni di grande fermento per la pallacanestro, ricordo che noi svolgevamo tutti i campionati maschili e femminili con ottimi risultati. Pio Zizi era il vero motore dell’associazione: attorno a lui iniziavano a crescere nuovi coaches e istruttori. Rosa Ruocco fu ceduta a Gragnano nella A1 femminile e, in seguito, proseguendo l’ottima tradizione femminile, anche Liliana Miccio, classe 1989, sta svolgendo un’ottima carriera in Serie A ed è attualmente top player di Firenze in A2.  
+        Dopo Giovanni Ruocco, fu eletto presidente Gennaro Apicella, padre di Carmela, che militava nella squadra di Serie C, e fu proprio lui a indurmi a deporre la casacca di giocatore per intraprendere la carriera di coach, che continuo a svolgere ancora oggi con grande passione e divertimento: siamo nel lontano 1980.  
+        In quest’anno ci fu anche un accordo con il C.S. Amalfitano per unire le forze e partecipare insieme a tutti i campionati giovanili e senior. Quell’anno, infatti, prendemmo parte a tutti i campionati maschili e femminili, schierando ben due squadre per ogni categoria. Si contavano circa 600 atlete e atleti che giocavano a pallacanestro in Costiera. Purtroppo, l’accordo non andò avanti, ma gli anni Ottanta videro comunque il G.S. Minori protagonista in ambito provinciale e regionale."
       ),
       array(
-        "title" => "I nuovi presidenti e l’eredità del G.S. Minori",
-        "content" => "Dopo Giovanni Ruocco, fu eletto presidente Gennaro Apicella, padre di Carmela (che militava nella squadra di Serie C), il quale è stato la persona che mi ha indotto a deporre la casacca di giocatore per intraprendere la carriera di coach, che continuo a svolgere ancora oggi con grande passione e divertimento. Siamo nel lontano 1980, e la passione per la pallacanestro a Minori non ha mai smesso di ardere.",   
+        "date" => "Metà Anni '80",
+        "title" => "La presidenza più giovane<br>e il ritorno di Gabriele Di Lieto",
+        "content" => "A metà degli anni Ottanta diventa presidente Rosario Costa, il più giovane presidente che l’associazione abbia mai avuto, sebbene per un breve periodo. In seguito subentrò di nuovo Gabriele Di Lieto, che lascerà la sua pesante eredità sulle spalle di Vito D’Alfonso.",   
          "image" => "photo_club/storia1.jpg"
-        )
+      ),
+      array(
+        "date" => "Inizio Anni '90",
+        "title" => "Un altro cambio di generazione",
+        "content" => "All’inizio degli anni Novanta, un’ulteriore svolta: Pio Zizzi, per motivi di lavoro, si trasferisce altrove, e con lui parte anche Aniello Di Lieto, che oltre a giocare svolgeva attività come istruttore di minibasket. Si affaccia così una nuova generazione alla guida tecnica della società. Peppino Fusco, che aveva iniziato ad allenare con me come assistente per la Promozione Maschile, diventa il perno dell’attività in quegli anni, portando la squadra maschile al primo storico accesso alla Serie D e allenando anche numerosi gruppi giovanili.  
+          Nel minibasket, oltre al fratello Marco Fusco, subentrarono come istruttori Vito D’Alfonso – attuale presidente – Emiliana Gambardella, Lina Andro e Tonino Di Pino. In questi anni arrivammo a disputare anche il campionato di Serie B femminile regionale e quello di Serie C maschile regionale."
+      ),
+      array(
+        "date" => "Fine Anni ’90",
+        "title" => "Una fase interlocutoria",
+        "content" => "A fine anni Novanta assistiamo a una fase interlocutoria, con un breve ritorno di Pio Zizzi. La Promozione Maschile è allenata da Christian De Juliis, affiancato da Ubaldo Troiano. ",
+      ),
+      array(
+        "date" => "Anni 2000",
+        "title" => "Nuovi coach e progetti ambiziosi",
+        "content" => "Dal 2000 in poi, la prima necessità è stata quella di formare nuovi allenatori in grado di portare avanti l’attività. Hanno collaborato con me Danilo Irace, poi in seguito Andrea Di Pino, Gennaro Infante, Vincenzo Ruocco e, in tempi più recenti, Jacopo Porpora e Gabriele Di Lieto (nipote e omonimo di uno dei tre ispiratori di quest’avventura che dura ancora oggi).  
+        Nel settore minibasket, dopo una lunga e proficua collaborazione di Irene Avellino, attualmente si sono distinti con ottimi risultati Federico Apicella, Andrea Di Pino e Giancarlo Di Bianco. Nel corso degli anni 2000, il G.S. Minori, continuando a esistere con il settore minibasket, ha aderito insieme alla Stella Azzurra Maiori e al C.S. Amalfitano a un affascinante progetto: la costituzione della Pallacanestro Costiera Amalfitana. Purtroppo, quest’iniziativa è naufragata per ragioni che non staremo qui a spiegare. "
+      ),
+      array(
+        "date" => "Oggi...",
+        "title" => "Una storia che continua",
+        "content" => "Mi trovo a essere, dopo 52 anni di partecipazione, il più anziano dell’associazione, ma il G.S. miracolosamente è ancora attivo perché è riuscito, in questi anni, ad avere costantemente un ricambio, con nuovi e giovani allenatori e dirigenti. "
+      )
     );
   ?>
 
@@ -144,7 +172,16 @@
         </div>
         <?php endforeach; ?>
       </div>
-     
+    <section id="autore" class="section-autore">
+    <h1>Autore</h1>
+    <div class="autore-content">
+      <img src="photo_club/mimmo.jpeg" alt="Foto dell'autore" class="autore-foto">
+      <p class="firma">
+        <em>Mimmo Apicella</em>
+      </p>
+
+  </section>
+
   </div>
 </section>
 
