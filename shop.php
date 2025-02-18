7874a28a8c8be002866666c89fc78fb0f03d603d
Di Marino Domenico <d.dimarino8@studenti.unisa.it>
2025-02-18 22:29:47 +0100

    tolto script inutilizzato in shop

--- shop.php
+++ shop.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="shop.css" type="text/css"/>
     <link rel="stylesheet" href="slideshow.css" type="text/css"/>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="search-container">
