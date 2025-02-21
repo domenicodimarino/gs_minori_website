c1d8b161019bef8e5c7fa9b3b63b852aca25103f
giovanniadinolfi <g.adinolfi39@studenti.unisa.it>
2025-02-21 10:43:15 +0100

    sql shop

--- SQL/product_inventory.sql
+++ SQL/product_inventory.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.product_inventory
 (
-    product_id integer NOT NULL DEFAULT nextval('product_inventory_product_id_seq'::regclass),
+    product_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     product_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
     price numeric(10,2) NOT NULL,
     available_quantity integer NOT NULL DEFAULT 0,
